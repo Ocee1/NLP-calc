6584a30a7673cc6903caa8242b57e3682bcb12a1
Ochapa James <ocjay24@gmail.com>
2022-11-05 21:10:28 +0100

    update

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 require('dotenv').config();
-const { Configuration, OpenAIApi } = require("openai");
+
 const cors = require('cors');
 
 
@@ -11,19 +11,16 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
-});
-const openai = new OpenAIApi(configuration);
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
 app.post('/', async (req, res) => {
-    
-    const { operation_type, x, y } = req.body;
-    if (operation_type == 'addition' || operation_type == 'add' || operation_type == '+') {
+    const y = req.body.y;
+    const x = req.body.x;
+    let opstring = req.body.operation_type;
+    if (opstring == 'addition' || opstring == 'add' || opstring == '+' || opstring.includes('addition') || opstring.includes('add')) {
         let result = x + y;
         res.json({
             "slackUsername": "Jake",
@@ -31,7 +28,7 @@ app.post('/', async (req, res) => {
             "operation_type": "addition"
         });
     }
-    else if (operation_type == 'subtraction' || operation_type == 'minus' || operation_type == '-') {
+    else if (opstring == 'subtraction' || opstring == 'minus' || opstring == '-' || opstring.includes('minus') || opstring.includes('subtraction') || opstring.includes('difference')) {
         let result = x - y;
         res.json({
             "slackUsername": "Jake",
@@ -39,7 +36,7 @@ app.post('/', async (req, res) => {
             "operation_type": "subtraction"
         });
     }
-    else if (operation_type == '*' || operation_type == 'multiplication' || operation_type == 'multiply') {
+    else if (opstring == '*' || opstring == 'multiplication' || opstring == 'multiply' || opstring.includes('divide') || opstring.includes('division')) {
         let result = x * y;
         res.json({
             "slackUsername": "Jake",
@@ -47,32 +44,6 @@ app.post('/', async (req, res) => {
             "operation_type": "multiplication"
         });
     } else {
-        let operation;
-        const response = await openai.createCompletion({
-            model: "text-davinci-002",
-            prompt: operation_type,
-            temperature: 0,
-            max_tokens: 100,
-            top_p: 1.0,
-            frequency_penalty: 0.2,
-            presence_penalty: 0.0,
-            stop: ["\n"],
-        });
-        let result = response;
-        let opstr = toString(response);
-        if (opstr.includes('+')) {
-            operation = '+';
-        } else if (opstr.includes('*')) {
-            operation = '*';
-        }else if (opstr.includes('-')) {
-            operation = '-';
-        } else {
-            operation = opstr;
-        }
-        res.json({
-            "slackUsername": "Jake",
-            "result": result,
-            "operation_type": operation
-        });
+        res.json({msg: "invalid operation"})
     }
 }).listen(PORT, () => console.log('Server is listening on port: ', PORT));
